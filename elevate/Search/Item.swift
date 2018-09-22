b384f9002cf831edf7e3e51858a54aa4234eca08
Richard Yu <richardbryu@gmail.com>
2018-09-22 17:46:28 -0400

    Added stub data and cells for search

--- elevate/Search/Item.swift
+++ elevate/Search/Item.swift
@@ -1,5 +1,27 @@
 struct Item: Codable {
-    var itemId: Int
+    var itemId: String
     var name: String
+    var description: String?
+    var imageName: String
     var price: Double
+    var shop: Shop
+
+    enum CodingKeys: String, CodingKey {
+        case itemId = "id"
+        case imageName = "image"
+        case name
+        case description
+        case price
+        case shop
+    }
+}
+
+struct Items: Codable {
+    var items: [Item]
+}
+
+struct Shop: Codable {
+    var shopId: String
+    var name: String
+    var distance: String
 }
